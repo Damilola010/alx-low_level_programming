657093f99975264f11bf6600a1134552ea8cf302
Damilola010 <dhamedrah@gmail.com>
2022-10-03 16:43:59 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,9 +4,11 @@
 /**
 * main - program that prints its name, followed by a new line
 * @argc: argument that counts argument input
-* @argv: argument that stores the strings in an array of char* (strings)
+* @argv: argument that stores the strings in an array of char
+* (strings)
 * Return: 0
 */
+
 int main(int argc, char *argv[])
 {
 int num_1, num_2, mul;
